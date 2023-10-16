ea3d007f4340c9776835ea4cd925ed38ca4470fa
AWOSZCZYK <85797503+AWOSZCZYK@users.noreply.github.com>
2023-10-17 01:34:07 +0200

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -15,7 +15,7 @@ public class Ex3 {
 
         int r = Integer.parseInt(s1, 16);
         int g = Integer.parseInt(s2, 16);
-        int b = Integer.parseInt(s2, 16);
+        int b = Integer.parseInt(s3, 16); // zamiana s2 na s3
         System.out.printf("(" + r + "," + b + "," + g + ")");
 
     }
