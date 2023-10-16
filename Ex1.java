3dc195dfecea4913686e06744960a05a0b858e7d
AWOSZCZYK <85797503+AWOSZCZYK@users.noreply.github.com>
2023-10-17 01:33:02 +0200

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -18,8 +18,8 @@ public class Ex1 {
         if (v0 < 0 || angle < 0 || angle > 90) {
             System.out.println("Nieprawidłowe dane wejściowe.");
         } else {
-            double distance = (Math.pow(v0, 2) * Math.sin(2 * angle)) / 9.81;
-            double maxHeight = (Math.pow(v0, 2) * Math.sin(2 * angle) * Math.sin(2 * angle) / (2 * 9.81));
+            double distance = (Math.pow(v0, 2) * Math.sin(2 * Math.toRadians(angle))) / 9.81; // konwersja kąta do radianów
+            double maxHeight = (Math.pow(v0, 2) * Math.sin(2 * Math.toRadians(angle)) * Math.sin(2 * Math.toRadians(angle)) / (2 * 9.81)); // konwersja kąta do radianów
 
             System.out.println("Zasięg: " + distance + " m");
             System.out.println("Maksymalna wysokość: " + maxHeight + " m");
